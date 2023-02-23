bfc281f882fec20e60b64c4a7a646a0b39d49505
eyxnz <todaytubuck@naver.com>
2023-02-23 11:44:15 +0900

    #7 - 카테고리 장소 검색 서비스 작성

--- src/main/java/com/convenience/conveniencestorerecommendation/service/api/KakaoUriBuilderService.java
+++ src/main/java/com/convenience/conveniencestorerecommendation/service/api/KakaoUriBuilderService.java
@@ -5,6 +5,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 
+import static com.convenience.conveniencestorerecommendation.constant.BaseURL.KAKAO_LOCAL_CATEGORY_SEARCH_URL;
 import static com.convenience.conveniencestorerecommendation.constant.BaseURL.KAKAO_LOCAL_SEARCH_ADDRESS_URL;
 
 @Service
@@ -20,4 +21,19 @@ public class KakaoUriBuilderService {
     }
 
     // 카테고리로 장소 검색하기 URI 만들기
+    public URI buildUriByCategorySearch(double latitude, double longitude, double radius, String category) {
+        // 카카오 API 는 미터 단위를 사용
+        double meterRadius = radius * 1000;
+
+        return UriComponentsBuilder
+                .fromHttpUrl(KAKAO_LOCAL_CATEGORY_SEARCH_URL.getUrl())
+                .queryParam("category_group_code", category)
+                .queryParam("x", longitude)
+                .queryParam("y", latitude)
+                .queryParam("radius", meterRadius)
+                .queryParam("sort","distance")
+                .build()
+                .encode()
+                .toUri();
+    }
 }
