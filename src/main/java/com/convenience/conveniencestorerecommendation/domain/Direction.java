904bb9f9bcb3725595ec4f96bbfbdaefcfbb2a2c
eyxnz <todaytubuck@naver.com>
2023-02-27 17:40:14 +0900

    #9 - Entity 설계
    입력 받은 사용자 주소와 근처 편의점의 정보를 담을 Entity 를 설계했다.

---
+++ src/main/java/com/convenience/conveniencestorerecommendation/domain/Direction.java
@@ -0,0 +1,35 @@
+package com.convenience.conveniencestorerecommendation.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Entity(name = "direction")
+public class Direction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 사용자
+    private String inputAddress;
+    private double inputLatitude;
+    private double inputLongitude;
+
+    // 편의점
+    private String targetConvenienceStoreName;
+    private String targetAddress;
+    private double targetLatitude;
+    private double targetLongitude;
+
+    // 사용자 주소 와 편의점 주소 사이의 거리
+    private double distance;
+}
---
+++ src/main/java/com/convenience/conveniencestorerecommendation/repository/DirectionRepository.java
@@ -0,0 +1,7 @@
+package com.convenience.conveniencestorerecommendation.repository;
+
+import com.convenience.conveniencestorerecommendation.domain.Direction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DirectionRepository extends JpaRepository<Direction, Long> {
+}
