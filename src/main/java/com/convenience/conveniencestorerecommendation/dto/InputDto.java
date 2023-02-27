f3d8a96ea0b8d24d70e7e12ab144709418c8f554
eyxnz <todaytubuck@naver.com>
2023-02-27 18:46:34 +0900

    #9 - InputDto 수정

--- src/main/java/com/convenience/conveniencestorerecommendation/dto/InputDto.java
+++ src/main/java/com/convenience/conveniencestorerecommendation/dto/InputDto.java
@@ -1,14 +1,10 @@
 package com.convenience.conveniencestorerecommendation.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class InputDto {
     private String address;
 }
